a82b28f8d289d550e9013f3d553d4b12424bff77
Guangchuang Yu <guangchuangyu@gmail.com>
2020-01-27 22:43:07 +0800

    scihub-dl

---
+++ R/scihub-dl.R
@@ -0,0 +1,24 @@
+##' download publication via scihub
+##' 
+##' using scihub to download publication using doi
+##' @rdname scihub-dl
+##' @name scihub_dl
+##' @param doi doi
+##' @param scihub scihub website
+##' @param download whether download the pdf file
+##' @return pdf url
+##' @author Guangchuang Yu
+##' @export
+scihub_dl <- function(doi, scihub = 'sci-hub.tw', download=TRUE) {
+    url <- paste0('https://', scihub, '/', doi)
+    x <- readLines(url)
+    i <- grep('id = "pdf"', x)
+
+    pdf_url <-sub(".*(//.*\\.pdf).*", "https:\\1", x[i])
+    
+    if (download) {
+        outfile <- sub(".*/", "", pdf_url)
+        download.file(pdf_url, destfile = outfile)
+    }
+    invisible(pdf_url)
+}
